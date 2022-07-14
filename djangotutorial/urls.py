c3a01b97c9e0be8d45f3c112d915ac5981ff0e46
Vít Němec <vitek2004@seznam.cz>
2022-07-14 07:25:09 +0100

    feat: Added names to paths #1

--- djangotutorial/urls.py
+++ djangotutorial/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
     path('users/<int:pk>/delete', UserDeleteView.as_view(), name="user-delete"),
 
     path('tasks/', TaskListView.as_view(), name="tasks-list"),
-    path('tasks/create', TaskCreateView.as_view()),
-    path('tasks/<int:pk>', TaskDetailView.as_view()),
-    path('tasks/<int:pk>/update', TaskUpdateView.as_view()),
-    path('tasks/<int:pk>/delete', TaskDeleteView.as_view())
+    path('tasks/create', TaskCreateView.as_view(), name="task-create"),
+    path('tasks/<int:pk>', TaskDetailView.as_view(), name="task-detail"),
+    path('tasks/<int:pk>/update', TaskUpdateView.as_view(), name="task-update"),
+    path('tasks/<int:pk>/delete', TaskDeleteView.as_view(), name="task-delete")
 ]
